b85972db2f302ed243f8ca1a59ba7d5520cc3dde
vipulfadtedev <vipulfadtedev@gmail.com>
2021-07-01 01:30:29 +0530

    auto length of x axis and show data for only last hour

--- src/components/line-chart.tsx
+++ src/components/line-chart.tsx
@@ -5,32 +5,32 @@ import axios from 'axios';
 const LineChart = () => {
 
     const [data, setData] = useState({});
-    const colorMap: {[index: string]:string} = {}
+    const colorMap: { [index: string]: string } = {}
     const [colorStore, setColorStore] = useState(colorMap);
 
     const getRandomColor = () => {
         var letters = '0123456789ABCDEF';
         var color = '#';
         for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
-      }
-    
-      const getColor = (counter: string) => {
+    }
+
+    const getColor = (counter: string) => {
         let tempColorStore = colorStore;
         let color = tempColorStore[counter];
-        if(!color){
-         color = getRandomColor();
-         tempColorStore[counter] = color
-         setColorStore(tempColorStore);
+        if (!color) {
+            color = getRandomColor();
+            tempColorStore[counter] = color
+            setColorStore(tempColorStore);
         }
         return color;
-      }
+    }
 
     useEffect(() => {
         const run = () => {
-            axios.get(`http://10.0.0.7:5000/stats`)
+            axios.get(`http://10.0.0.7:5000/stats/in-last-hour`)
                 .then(res => {
                     const status = res.data;
                     let myData: any = {}
@@ -42,12 +42,19 @@ const LineChart = () => {
                             myData[record.host] = [Number(record.value)];
                         }
                     });
-                    
+
+                    let maxLength = 0;
+                    const datasets = [];
+                    const labels = [];
+
                     for (const key in myData) {
                         myData[key] = myData[key].slice(Math.max(myData[key].length - 20, 0));
+                        maxLength = myData[key].length > maxLength ? myData[key].length : maxLength;
                     }
-                    const datasets = [];
-                    const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+                    
+                    for(let i=1; i<=maxLength; i++){
+                        labels.push(i);
+                    } 
 
                     for (const key in myData) {
                         datasets.push({
@@ -57,6 +64,7 @@ const LineChart = () => {
                             borderColor: getColor(key)
                         });
                     }
+
                     setData({
                         labels,
                         datasets
@@ -64,7 +72,7 @@ const LineChart = () => {
                 });
         }
         run()
-        const interval = setInterval(() => run(), 300000)
+        const interval = setInterval(() => run(), 60000)
         return () => {
             clearInterval(interval);
         }
