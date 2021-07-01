dc6b89ae857eae677f51545c9d58160c34659cf9
vipulfadtedev <vipulfadtedev@gmail.com>
2021-07-02 04:08:32 +0530

    restructured ui to show multiple counters as 1 graph each with time on x axis..

--- src/components/line-chart.tsx
+++ src/components/line-chart.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import axios from 'axios';
+import 'chartjs-adapter-moment';
 
 const LineChart = () => {
 
-    const [data, setData] = useState({});
+    const [data, setData] = useState([] as any);
     const colorMap: { [index: string]: string } = {}
     const [colorStore, setColorStore] = useState(colorMap);
 
@@ -28,47 +29,62 @@ const LineChart = () => {
         return color;
     }
 
+    const epochToDate = (epoch: number) => {
+        if (epoch < 10000000000)
+            epoch *= 1000;
+        epoch = epoch + (new Date().getTimezoneOffset() * -1);
+        return new Date(epoch);
+    }
+
     useEffect(() => {
         const run = () => {
+            setData([]);
             axios.get(`http://10.0.0.7:5000/stats/in-last-hour`)
-                .then(res => {
+                .then((res: { data: any; }) => {
                     const status = res.data;
-                    let myData: any = {}
 
-                    status.forEach((record: { host: string; value: string; }) => {
-                        if (myData[record.host]) {
-                            myData[record.host].push(Number(record.value));
+                    let statusData: any = {}
+                    status.forEach((record: { counter: string; host: string; value: string; timestamp: number; }) => {
+                        const counterName = record.counter;
+                        let counter = statusData[counterName];
+                        if (counter) {
+                            if (counter[record.host]) {
+                                counter[record.host].push({ x: epochToDate(record.timestamp), y: Number(record.value) });
+                            } else {
+                                counter[record.host] = [{ x: epochToDate(record.timestamp), y: Number(record.value) }];
+                            }
                         } else {
-                            myData[record.host] = [Number(record.value)];
+                            statusData[record.counter] = {}
+                            statusData[record.counter][record.host] = [{ x: epochToDate(record.timestamp), y: Number(record.value) }];
                         }
                     });
 
-                    let maxLength = 0;
-                    const datasets = [];
-                    const labels = [];
+                    for (const counter in statusData) {
+                        const datasets = [];
 
-                    for (const key in myData) {
-                        myData[key] = myData[key].slice(Math.max(myData[key].length - 20, 0));
-                        maxLength = myData[key].length > maxLength ? myData[key].length : maxLength;
+                        for (const key in statusData[counter]) {
+                            datasets.push({
+                                label: key,
+                                data: statusData[counter][key],
+                                backgroundColor: getColor(key),
+                                borderColor: getColor(key)
+                            });
+                        }
+
+                        const options = {
+                            scales: {
+                                x: {
+                                    type: 'time',
+                                    time: {
+                                        displayFormats: {
+                                            second: 'DD-MM-YY hh:mm:ss'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        setData([...data, { options, data: { datasets }, counter: counter.toUpperCase() }]);
                     }
-                    
-                    for(let i=1; i<=maxLength; i++){
-                        labels.push(i);
-                    } 
-
-                    for (const key in myData) {
-                        datasets.push({
-                            label: key,
-                            data: myData[key],
-                            backgroundColor: getColor(key),
-                            borderColor: getColor(key)
-                        });
-                    }
-
-                    setData({
-                        labels,
-                        datasets
-                    });
                 });
         }
         run()
@@ -76,11 +92,19 @@ const LineChart = () => {
         return () => {
             clearInterval(interval);
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div>
-            <Line type='line' data={data} />
+            {
+                data.map((value: any, index: string) => {
+                    return <div key={index}>
+                        <h5 key={value.counter}>{value.counter} Graph: </h5>
+                        <div key={index}> <Line type='line' data={value.data} options={value.options} /></div>
+                    </div>
+                })
+            }
         </div>
     );
 };
